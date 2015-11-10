9c36db4b1d4740a704545b615fd1c57dd68d54b0
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-10 21:48:57 +0200

    Revert "Moved routes.php"
    This reverts commit 94d36433fb7765889afabecc54d6694941a316a2.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,20 +1,16 @@
 <?php
 
-	Route::get('/', function()
-    {
-        return View::make('home');
-    });
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
 
-    // about page (app/views/users.blade.php)
-    Route::get('users', function()
-    {
-        return View::make('users');
-    });	
-	
-	App::missing(function($exception)
-    {
-
-        // shows an error page (app/views/error.blade.php)
-        // returns a page not found error
-        return Response::view('error', array(), 404);
-    });
+Route::get('/', function () {
+    return view('welcome');
+});
