6dbac6e6ebf52e31dd0f76dde174f648f92d1ff3
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-10 22:46:50 +0200

    Removed almost everything from users view

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,12 +1,6 @@
 <?php
 
-	$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-	echo "Actual";
-	echo $actual_link;
-	echo "Host";
-	echo $_SERVER[HTTP_HOST];
-	echo "Request";
-	echo $_SERVER[REQUEST_URI];
+	echo 'users';
 	
 
    
