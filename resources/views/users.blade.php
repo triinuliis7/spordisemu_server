fb24eb0064e8065468c1570a0213002feebdc5d3
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-10 22:37:23 +0200

    Changed users view

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,3 +1,20 @@
 <?php
 
-    echo 'users';
+    include 'database.php';
+    
+    $sql =<<<EOF
+        SELECT * from users;
+EOF;
+
+	$ret = pg_query($db, $sql);
+	if(!$ret){
+	  echo pg_last_error($db);
+	  exit;
+	} 
+	while($row = pg_fetch_row($ret)){
+	  echo "user_id = ". $row[0] . "\n";
+	  echo "username = ". $row[1] ."\n";
+	  echo "password = ". $row[2] ."\n";
+	}
+	echo "Operation done successfully\n";
+	pg_close($db);
