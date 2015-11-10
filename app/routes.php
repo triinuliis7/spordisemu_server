1cc79b62bc1ab33ef8511bac20c4af355b1e662e
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-10 21:24:35 +0200

    Routing

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,20 @@
 <?php
 
-    Route::resource('users', 'UsersController');
+	Route::get('/', function()
+    {
+        return View::make('home');
+    });
+
+    // about page (app/views/users.blade.php)
+    Route::get('users', function()
+    {
+        return View::make('users');
+    });	
+	
+	App::missing(function($exception)
+    {
+
+        // shows an error page (app/views/error.blade.php)
+        // returns a page not found error
+        return Response::view('error', array(), 404);
+    });
