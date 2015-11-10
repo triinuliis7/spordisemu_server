97954f47213d0300f4dc63e68bcb2619561df0bc
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-10 21:49:06 +0200

    Revert "Routing"
    This reverts commit 1cc79b62bc1ab33ef8511bac20c4af355b1e662e.

--- app/routes.php
+++ app/routes.php
@@ -1,20 +1,3 @@
 <?php
 
-	Route::get('/', function()
-    {
-        return View::make('home');
-    });
-
-    // about page (app/views/users.blade.php)
-    Route::get('users', function()
-    {
-        return View::make('users');
-    });	
-	
-	App::missing(function($exception)
-    {
-
-        // shows an error page (app/views/error.blade.php)
-        // returns a page not found error
-        return Response::view('error', array(), 404);
-    });
+    Route::resource('users', 'UsersController');
