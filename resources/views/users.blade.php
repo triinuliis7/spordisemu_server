7cd71f25d8161c27a5e99bb4094cff3dc55e831a
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-10 22:44:42 +0200

    Removed database stuff from user view

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -8,17 +8,6 @@
 	echo "Request";
 	echo $_SERVER[REQUEST_URI];
 	
-	
-   $host        = "host=ec2-54-163-228-109.compute-1.amazonaws.com";
-   $port        = "port=5432";
-   $dbname      = "dbname=d1h5f179s0jvci";
-   $credentials = "user=pnihhawbybmwbv password=UtOUDwOTDtieWfqFt7XZhffMnR";
 
-   $db = pg_connect( "$host $port $dbname $credentials"  );
-   if(!$db){
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "Opened database successfully\n";
-   }
    
    
