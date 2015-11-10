54e626464dae1ae04885bd62fa190bbc0ee4fe73
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-10 22:43:01 +0200

    User view changes

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -21,19 +21,4 @@
       echo "Opened database successfully\n";
    }
    
-   $sql =<<<EOF
-      SELECT * from users;
-EOF;
-
-   $ret = pg_query($db, $sql);
-   if(!$ret){
-      echo pg_last_error($db);
-      exit;
-   } 
-   while($row = pg_fetch_row($ret)){
-      echo "user_id = ". $row[0] . "\n";
-      echo "username = ". $row[1] ."\n";
-      echo "password = ". $row[2] ."\n";
-   }
-   echo "Operation done successfully\n";
-   pg_close($db);
+   
