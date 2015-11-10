b393f073abd494dfaa4d373a9b44a05d27a9be50
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-10 21:10:11 +0200

    Crud

---
+++ app/views/users/index.blade.php
@@ -0,0 +1,19 @@
+<?php
+
+	include 'database.php';
+	
+	$sql =<<<EOF
+		SELECT * from users;
+EOF;
+
+	$ret = pg_query($db, $sql);
+	if(!$ret){
+	  echo pg_last_error($db);
+	  exit;
+	}
+	$myarray = array()
+	while ($row = pg_fetch_row($ret)) {
+	  $myarray[] = $row;
+	}
+
+	echo json_encode($myarray);
