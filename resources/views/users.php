faefd8e075618fc6249f3450c0b1bae4aea0791c
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-11 18:47:03 +0200

    Updated SQL in show one user

--- resources/views/users.php
+++ resources/views/users.php
@@ -5,10 +5,9 @@
 	if(isset($username))
 	{		
 		echo $username;
-		$sql = "SELECT * from users where username=$username";
+		$sql = "SELECT * from users where username=", $username;
 	}
 	else {
-		echo "hei";
 		$sql = "SELECT * from users";
 	}
 
