e8ce34ef4e4fe7fd2b3fc30961848d04caa6c4c6
Triinu Liis Kelder <triinuk@proekspert.ee>
2015-11-16 08:57:19 +0200

    Added print_r

--- resources/views/users.php
+++ resources/views/users.php
@@ -10,6 +10,7 @@
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $json = file_get_contents('php://input');
             $obj = json_decode($json);
+            print_r($obj);
             echo "POST";
             $user = $obj["firstname"];
             $sql = "SELECT * from users where username='kalle123'";
