8c00e908f54ba82caf7da82e70fb3e5b5d05e847
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-24 19:31:46 +0200

    Fixed delete attend sql

--- resources/views/attends.php
+++ resources/views/attends.php
@@ -12,7 +12,7 @@
                 VALUES ('$user_id', '$practice_id')
                 RETURNING attend_id, user_id, practice_id";
     } else if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
-        $sql = "DELETE FROM attends where attend_id='$attend_id"
+        $sql = "DELETE FROM attends where attend_id='$attend_id'";
     } else {
         $sql = "SELECT * FROM attends where practice_id='$practice_id'";
     }
