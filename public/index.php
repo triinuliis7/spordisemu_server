38e184d4dfa230b4ed00cd48e02b097ab726b09a
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-26 14:20:43 +0200

    Trying Parse notifications

--- public/index.php
+++ public/index.php
@@ -7,6 +7,17 @@
  * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
 
+/* --- PARSE NOTIFICATIONS --- */
+require 'vendor/autoload.php';
+ 
+use Parse\ParseClient;
+ 
+ParseClient::initialize('FWLLWZk8Adonl3ixkHu71nPUDaM1R2uFcmZJKQA5', 'hbWpEyWaIIMnmDfFW2wOBGAuEWWm9Af6iUlEIn60', 'RzTrmMinir7BJMxFt4kUinaeKWDbRZ7bQb1ZZeqv');
+ 
+$testObject = ParseObject::create("TestObject");
+$testObject->set("foo", "bar");
+$testObject->save();
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
