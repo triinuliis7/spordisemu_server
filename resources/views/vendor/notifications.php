777495606d556773f8c862cfb8a583a6608aafb0
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-26 16:17:02 +0200

    Trying new php in notifications

--- resources/views/vendor/notifications.php
+++ resources/views/vendor/notifications.php
@@ -2,13 +2,19 @@
 
 $APPLICATION_ID = "FWLLWZk8Adonl3ixkHu71nPUDaM1R2uFcmZJKQA5";
 $REST_API_KEY = "hbWpEyWaIIMnmDfFW2wOBGAuEWWm9Af6iUlEIn60";
-$MESSAGE = "your-alert-message";
+
 $url = 'https://api.parse.com/1/push';
-$_data = '{"where":"{}",
-    "data": { "alert": "greetings programs" } }';
+$data = array(
+    'while' => '{}',
+    'expiry' => 1451606400,
+    'data' => array(
+        'alert' => 'greetings programs',
+    ),
+);
+$_data = json_encode($data);
 $headers = array(
-    'X-Parse-Application-Id: FWLLWZk8Adonl3ixkHu71nPUDaM1R2uFcmZJKQA5',
-    'X-Parse-REST-API-Key: hbWpEyWaIIMnmDfFW2wOBGAuEWWm9Af6iUlEIn60',
+    'X-Parse-Application-Id: ' . $APPLICATION_ID,
+    'X-Parse-REST-API-Key: ' . $REST_API_KEY,
     'Content-Type: application/json',
     'Content-Length: ' . strlen($_data),
 );
@@ -17,4 +23,5 @@ $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_POST, 1);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $_data);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 curl_exec($curl);
