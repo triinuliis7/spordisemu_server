b6c9e6f0ec947deaf4dcff9ed68ee640b11ad99f
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-26 16:24:49 +0200

    Trying new curl

--- resources/views/vendor/notifications.php
+++ resources/views/vendor/notifications.php
@@ -1,27 +1,23 @@
 <?php
 
-$APPLICATION_ID = "FWLLWZk8Adonl3ixkHu71nPUDaM1R2uFcmZJKQA5";
-$REST_API_KEY = "hbWpEyWaIIMnmDfFW2wOBGAuEWWm9Af6iUlEIn60";
+$vars = '{"where":"{}",
+    "data": { "alert": "greetings programs" } }';
 
-$url = 'https://api.parse.com/1/push';
-$data = array(
-    'while' => '{}',
-    'expiry' => 1451606400,
-    'data' => array(
-        'alert' => 'greetings programs',
-    ),
-);
-$_data = json_encode($data);
-$headers = array(
-    'X-Parse-Application-Id: ' . $APPLICATION_ID,
-    'X-Parse-REST-API-Key: ' . $REST_API_KEY,
-    'Content-Type: application/json',
-    'Content-Length: ' . strlen($_data),
-);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"http://api.parse.com/1/push");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$vars);  //Post Fields
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $_data);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-curl_exec($curl);
+$headers = array();
+$headers[] = 'X-Parse-Application-Id: FWLLWZk8Adonl3ixkHu71nPUDaM1R2uFcmZJKQA5';
+$headers[] = 'X-Parse-REST-API-Key: hbWpEyWaIIMnmDfFW2wOBGAuEWWm9Af6iUlEIn60';
+$headers[] = 'Content-Type: application/json';
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$server_output = curl_exec ($ch);
+
+curl_close ($ch);
+
+print  $server_output ;
