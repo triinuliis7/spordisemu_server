05eeb070be8190ad020d7ac022e9ea8c7bbce44b
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-26 14:52:42 +0200

    Trying something new

--- resources/views/vendor/notifications.php
+++ resources/views/vendor/notifications.php
@@ -1,11 +1,27 @@
 <?php
 
-require 'vendor/autoload.php';
- 
-use Parse\ParseClient;
- 
-ParseClient::initialize('FWLLWZk8Adonl3ixkHu71nPUDaM1R2uFcmZJKQA5', 'hbWpEyWaIIMnmDfFW2wOBGAuEWWm9Af6iUlEIn60', 'RzTrmMinir7BJMxFt4kUinaeKWDbRZ7bQb1ZZeqv');
+$APPLICATION_ID = FWLLWZk8Adonl3ixkHu71nPUDaM1R2uFcmZJKQA5;
+$REST_API_KEY = hbWpEyWaIIMnmDfFW2wOBGAuEWWm9Af6iUlEIn60;
 
-$testObject = ParseObject::create("TestObject");
-$testObject->set("foo", "bar");
-$testObject->save();
+$url = 'https://api.parse.com/1/push';
+$data = array(
+    'channel' => '',
+    'type' => 'android',
+    'expiry' => 1451606400,
+    'data' => array(
+        'alert' => 'greetings programs',
+    ),
+);
+$_data = json_encode($data);
+$headers = array(
+    'X-Parse-Application-Id: ' . $APPLICATION_ID,
+    'X-Parse-REST-API-Key: ' . $REST_API_KEY,
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($_data),
+);
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $_data);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_exec($curl);
