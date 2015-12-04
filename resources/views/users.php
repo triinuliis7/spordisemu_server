a03775b42249c750202d0a047427c7d4453ff672
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-12-04 16:16:19 +0200

    Fixed change password

--- resources/views/users.php
+++ resources/views/users.php
@@ -4,30 +4,30 @@
 	
 	if (isset($username))
     {
-        $sql = "SELECT * FROM users where username='$username'";
-    } else if (isset($user_id)) {
-        $sql = "SELECT * FROM users where id='$user_id'";
-    } else {
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (isset($username)) {
-                $json = file_get_contents('php://input');
+            $json = file_get_contents('php://input');
                 $json = json_decode($json, true);
                 $password = $json['password'];
                 $sql = "UPDATE users SET password='$password' WHERE username='$username' 
                         RETURNING id, username, password";
-            } else {
-                $json = file_get_contents('php://input');
-                $json = json_decode($json, true);
-                $firstname = $json['firstname'];
-                $lastname = $json['lastname'];
-                $username = $json['username'];
-                $gender = $json['gender'];
-                $email = $json['email'];
-                $password = $json['password'];
-                $sql = "INSERT INTO users (firstname, lastname, username, gender, email, password) 
-                        VALUES ('$firstname', '$lastname', '$username', '$gender', '$email', '$password') 
-                        RETURNING id, firstname, lastname, username, gender, email, password";
-            }
+        } else {
+            $sql = "SELECT * FROM users where username='$username'";
+        }
+    } else if (isset($user_id)) {
+        $sql = "SELECT * FROM users where id='$user_id'";
+    } else {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $json = file_get_contents('php://input');
+            $json = json_decode($json, true);
+            $firstname = $json['firstname'];
+            $lastname = $json['lastname'];
+            $username = $json['username'];
+            $gender = $json['gender'];
+            $email = $json['email'];
+            $password = $json['password'];
+            $sql = "INSERT INTO users (firstname, lastname, username, gender, email, password) 
+                    VALUES ('$firstname', '$lastname', '$username', '$gender', '$email', '$password') 
+                    RETURNING id, firstname, lastname, username, gender, email, password";
         } else {
             $sql = "SELECT * from users";
         }
